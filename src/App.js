61267606455e60089c56f16f490aa9c191cab6ba
Udit Chaudhary <sagunchaudhary01234@gmail.com>
2024-04-07 18:19:08 +0530

    tic-tac

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Tictac from './components/tic tac/Tic tac';
+
+function App() {
+  return (
+    <div className="App">
+      <Tictac/>
+    </div>
+  );
+}
+
+export default App;
